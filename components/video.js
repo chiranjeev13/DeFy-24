b165179a6cafa73542a9adf852dbbb29e27ebe06
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-11-01 12:39:32 +0530

    fixes

--- components/video.js
+++ components/video.js
@@ -3,27 +3,21 @@ import styles from "../styles/video.module.css";
 import "@vidstack/react/player/styles/default/theme.css";
 import "@vidstack/react/player/styles/default/layouts/video.css";
 import { MediaPlayer, MediaProvider } from "@vidstack/react";
-import {
-  defaultLayoutIcons,
-  DefaultVideoLayout,
-} from "@vidstack/react/player/layouts/default";
 
 function Video() {
   return (
     <>
       <div className={styles.video}>
         <MediaPlayer
-          title="Sprite Fight"
           src="https://upcdn.io/FW25bjZ/raw/defypromo.mp4"
-          autoplay
-          volume={0.5}
+          muted={true}
+          loop={true}
+          autoplay={true}
+          playbackRate={1.75}
+          aspectRatio=""
         >
           <MediaProvider />
-          <DefaultVideoLayout
-            icons={defaultLayoutIcons}
-          />
         </MediaPlayer>
-        ;{" "}
       </div>
     </>
   );
