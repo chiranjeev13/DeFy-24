02c99ed4fbea5e8eeff759f1f65ef4bb9643ec71
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-11-05 14:04:58 +0530

    more edits card added

--- components/body.jsx
+++ components/body.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/body.module.css";
+import Card from "./card";
+
+
 function Body() {
   return (
     <>
       <div className={styles.body}>
         Core Concept of
         <div>DeFy</div>
-        <div className="relative h-48 w-48 overflow-hidden rounded-xl border border-slate-800 p-[1px] backdrop-blur-3xl">
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <div className="inline-flex h-full w-full items-center justify-center rounded-xl bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl" />
-        </div>
+        <Card />
       </div>
     </>
   );
---
+++ components/card.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import styles from "../styles/card.module.css";
+import logo from "../assets/defy-logo.svg";
+import Image from "next/image";
+function Card() {
+  return (
+    <>
+      <div className={styles.card}>
+        <Image src={logo}/>
+        <p className={styles.card_content}>
+        DeFy 24 is a gripping 36 hour hackathon that unites Web3 enthusiasts and aspiring entrepreneurs at the college level, on a quest for real innovation. As the name suggests - DeFy 24 is unlike your typical collegiate hackathons, where the emphasis lies only on programming a tech solution. 
+
+Here, we challenge the status quo and redefine what a hackathon can be. It's not just about coding; it's about disrupting and reimagining the future. DeFy 24 aims to break boundaries, merging technology and business acumen to create Web3-powered startups that propel us to an era where ‘Decentralization’ and ‘Future yield’ go hand in hand. 
+
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Card;
