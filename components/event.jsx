02f8e5107c7a1a76a09d9d91a283e0b37cbb77d6
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-11-08 12:50:29 +0530

    added Event timeline cards

--- components/event.jsx
+++ components/event.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import styles from "../styles/event.module.css"
+import styles from "../styles/event.module.css";
 
-function eventcard({body , title})
-{
-    return(
-        <>
-        <div className={styles.card}>
-
-        </div>
-        </>
-    )
+function eventcard({ body, title }) {
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.title}>ROUND 1</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        reprehenderit nostrum, esse ullam ad libero quisquam, magni, incidunt
+        aspernatur repellendus impedit. Sapiente consequatur rerum alias
+        doloremque modi ducimus omnis! Aut.
+      </div>
+    </>
+  );
 }
 
-export default eventcard
+export default eventcard;
