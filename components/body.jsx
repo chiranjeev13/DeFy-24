1939af1bd252cacaa656664aa494e9204c5e557b
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-11-10 01:21:37 +0530

    devfolio intg

--- components/body.jsx
+++ components/body.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/body.module.css";
 import Card from "./card";
 
 function Body() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <>
       <section id="home">
         <div>
           <div className={styles.bodyy}>
             Core Concept of DeFy
+            <div
+              className="apply-button"
+              data-hackathon-slug="defy24"
+              data-button-theme="light"
+              style={{ height: "44px", width: "312px" }}
+            ></div>
             <Card />
           </div>
         </div>
--- components/card.jsx
+++ components/card.jsx
@@ -7,16 +7,7 @@ import { MediaPlayer, MediaProvider } from "@vidstack/react";
 
 function Card() {
   const [cardChange, setCardChange] = useState(0);
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://apply.devfolio.co/v2/sdk.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  
   return (
     <div className={styles.layout}>
       {cardChange === 0 && (
@@ -44,12 +35,7 @@ function Card() {
               speed={95}
             />
           </p>
-          <div
-            className="apply-button"
-            data-hackathon-slug="defy24"
-            data-button-theme="light"
-            style={{ height: "44px", width: "312px" }}
-          ></div>
+          
         </div>
       )}
 
