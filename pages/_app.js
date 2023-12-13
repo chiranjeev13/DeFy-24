cfc6795b54b375cc9a8e68a5dbe50418c5a40192
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-12-13 14:05:32 +0530

    feat: improved seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { Helmet } from "react-helmet";
 import "@/styles/globals.css";
 import Header from "@/components/Headerr";
@@ -7,31 +7,59 @@ import Header from "@/components/Headerr";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Helmet>
-        {/* Primary Meta Tags */}
-        <title>DeFy'24</title>
-        <meta name="description" content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist." />
-        
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
+      <Head>
+        {/* Meta Tags */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
+        />
+        {/* Add other meta tags, title, etc. here */}
+
+        {/* Open Graph Meta Tags (for social media sharing) */}
+        <meta property="og:title" content="DeFy'24" />
+        <meta
+          property="og:description"
+          content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
+        />
+        <meta
+          property="og:image"
+          content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
+        />
         <meta property="og:url" content="https://www.defy24.xyz/" />
-        <meta property="og:title" content="DeFy'24 | The Biggest Web3 Hackathon in Chennai" />
-        <meta property="og:description" content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist." />
-        <meta property="og:image" content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500" />
+        <meta property="og:type" content="website" />
 
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.defy24.xyz/" />
-        <meta property="twitter:title" content="DeFy'24 | The Biggest Web3 Hackathon in Chennai" />
-        <meta property="twitter:description" content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist." />
-        <meta property="twitter:image" content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500" />
+        {/* Twitter Meta Tags (for Twitter Card) */}
+        <meta
+          name="twitter:card"
+          content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
+        />
+        <meta name="twitter:title" content="DeFy'24" />
+        <meta
+          name="twitter:description"
+          content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
+        />
+        <meta
+          name="twitter:image"
+          content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
+        />
 
-        {/* Additional Meta Tags */}
-        <meta name="keywords" content="defy, defy24, defy'24, chennai, web3, web3 hackathon, chennai hackathon, web3 chennai, ethindia, dao, daocommunity, daovitchennai, vitchennai, vellore institute of technology" />
-        <meta name="author" content="Your Name" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
 
-      </Helmet>
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* CSS Reset or Normalize.css */}
+        {/* <link rel="stylesheet" href="path-to-reset-or-normalize.css" /> */}
+
+        {/* Your custom stylesheets or fonts */}
+        {/* <link rel="stylesheet" href="path-to-your-custom-styles.css" /> */}
+
+        {/* Title */}
+        <title>DeFy'24</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Analytics />
--- pages/_document.js
+++ pages/_document.js
@@ -3,69 +3,60 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Meta Tags */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
+        />
+        {/* Add other meta tags, title, etc. here */}
 
-      {/* Primary Meta Tags */}
-      <title>DeFy'24</title>
-      <meta
-        name="description"
-        content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
-      />
+        {/* Open Graph Meta Tags (for social media sharing) */}
+        <meta property="og:title" content="DeFy'24" />
+        <meta
+          property="og:description"
+          content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
+        />
+        <meta
+          property="og:image"
+          content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
+        />
+        <meta property="og:url" content="https://www.defy24.xyz/" />
+        <meta property="og:type" content="website" />
 
-      {/* Open Graph / Facebook */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.defy24.xyz/" />
-      <meta
-        property="og:title"
-        content="DeFy'24 | The Biggest Web3 Hackathon in Chennai"
-      />
-      <meta
-        property="og:description"
-        content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
-      />
-      <meta
-        property="og:image"
-        content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
-      />
+        {/* Twitter Meta Tags (for Twitter Card) */}
+        <meta
+          name="twitter:card"
+          content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
+        />
+        <meta name="twitter:title" content="DeFy'24" />
+        <meta
+          name="twitter:description"
+          content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
+        />
+        <meta
+          name="twitter:image"
+          content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
+        />
 
-      {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://www.defy24.xyz/" />
-      <meta
-        property="twitter:title"
-        content="DeFy'24 | The Biggest Web3 Hackathon in Chennai"
-      />
-      <meta
-        property="twitter:description"
-        content="DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist."
-      />
-      <meta
-        property="twitter:image"
-        content="https://pbs.twimg.com/profile_banners/1711670743749230592/1701195199/1500x500"
-      />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
 
-      {/* Additional Meta Tags */}
-      <meta
-        name="keywords"
-        content="defy, defy24, defy'24, chennai, web3, web3 hackathon, chennai hackathon, web3 chennai, ethindia, dao, daocommunity, daovitchennai, vitchennai, vellore institute of technology"
-      />
-      <meta name="author" content="Your Name" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
+        {/* CSS Reset or Normalize.css */}
+        {/* <link rel="stylesheet" href="path-to-reset-or-normalize.css" /> */}
 
-      {/* <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "http://schema.org",
-            "@type": schemaType,
-            name: "DeFy'24",
-            about: "DeFy'24 stands out from typical college hackathons by prioritizing fresh product ideas in the Web3 space. We're on the lookout for innovative startup concepts that leverage Web3 technology, emphasizing a future where 'decentralization' and 'future yield' coexist.",
-            url: "https://www.defy24.xyz/",
-          }),
-        }}
-      /> */}
-      
+        {/* Your custom stylesheets or fonts */}
+        {/* <link rel="stylesheet" href="path-to-your-custom-styles.css" /> */}
+
+        {/* Title */}
+        <title>DeFy'24</title>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
