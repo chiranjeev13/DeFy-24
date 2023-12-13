c7fd94ee0e81c3753d4fa2edd34887aa9aef694f
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-12-13 12:00:44 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Analytics } from '@vercel/analytics/react';
+
 
 import "@/styles/globals.css";
 import Header from "@/components/Headerr";
@@ -8,6 +10,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Header/>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
