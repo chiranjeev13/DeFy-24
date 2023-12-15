5dfadd6d4dd55d88bf2b50bbc064914e3d26e5a1
Rumman <rummanfardeen4567@gmail.com>
2023-12-15 19:12:58 +0530

    Button created for devfolio

---
+++ components/ButtonAnimatedGradient.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const ButtonAnimatedGradient = () => {
+    return (
+        <>
+            <a href="https://defy2024.devfolio.co/" className='text-md mt-3 py-10 font-bold transition-background inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-gradient-to-r from-slate-100 via-[#c5c4c5] to-[#7cfc00] bg-[length:200%_200%] bg-[0%_0%] px-6 text-black duration-500 hover:bg-[100%_200%] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ring ring-[#7cfc00] ring-opacity-50 backdrop-filter backdrop-blur-lg'>
+                Apply Today
+            </a>
+
+
+        </>
+    );
+};
+
+export default ButtonAnimatedGradient;
