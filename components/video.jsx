b9a20826c195fb672ac7ef7c10f943c2d0bf1897
Chiranjeev Mishra <44394108+chiranjeev13@users.noreply.github.com>
2024-06-27 13:07:32 +0530

    Update video.jsx

--- components/video.jsx
+++ components/video.jsx
@@ -4,14 +4,14 @@ import "@vidstack/react/player/styles/default/theme.css";
 import "@vidstack/react/player/styles/default/layouts/video.css";
 import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import AnimatedScrollButton from '@/components/AnimatedScrollButton';
-import video from "../assets/defypromo.mp4"
+//import video from "../assets/defypromo.mp4"
 
 function Video() {
   return (
     <div className={styles.videoWrapper}>
       <MediaPlayer
         className={styles.video}
-        src={video}
+        src="https://defy24.mos.ap-southeast-3.sufybkt.com/defypromo%20(1).mp4"
         muted={true}
         loop={true}
         autoplay={true}
